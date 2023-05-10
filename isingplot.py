357df5659632baa02144afb67ac68d65c293e95a
prajitbaruah <prajitbaruah@gmail.com>
2023-05-10 17:59:41 +0200

    Curve fitting

--- isingplot.py
+++ isingplot.py
@@ -1,10 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
 
 particle = 100*100
-file = open("equi_test.dat",  "r")
-data_meas = np.genfromtxt(file)
-file.close()
+data_meas = np.loadtxt('equi_test.dat')
 average_mag = data_meas[:, 1]
 print(average_mag[1])
 tmax = 59900000
@@ -29,9 +28,31 @@ auto_cor = []
 for i in range(0, int(tmax/step), 1):
     auto_cor.append(auto_correlation_function(i))
 auto_cor_normalized = np.array(auto_cor)/auto_cor[0]
+
+def exp_decay(x, a, b):
+    return a * np.exp(-b * x)
+
 plt.figure()
 plt.title("auto-cor")
 time = np.arange(0, tmax, step)
 plt.grid()
-plt.plot(time, np.log(auto_cor_normalized), '.')
+plt.scatter(time, np.log(auto_cor_normalized), s = 1)
 plt.show()
+
+
+
+y = np.log(auto_cor_normalized)
+
+y[np.isnan(y)] = np.nanmean(y)
+y[np.isinf(y)] = np.nanmedian(y)
+
+popt, pcov = curve_fit(exp_decay, time, y)
+
+a_opt = popt[0]
+b_opt = popt[1]
+
+x = (-np.log(0.5) / b_opt)
+in_x = (1/x)*tmax
+
+print(in_x)
+
